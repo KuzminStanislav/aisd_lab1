9ac6e2cc473a3359d218490a466c29294240b77a
KuzminStanislav <a663ve@gmail.com>
2024-11-12 08:19:45 +0400

    Add header geom_elements

---
+++ geom_elements.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <iostream>
+
+template <typename T>
+class Point {
+private:
+	float x, y;
+
+public:
+	Point(float x, float y);
+};
+
+template <typename T>
+class Line {
+private:
+	Point<T> p1, p2;
+
+public:
+	Line(Point<T> point1, Point<T> point2);
+
+	bool is_above(const Point<T>& point) const;
+};
+
